ae4a153de064a7a3dfa878fcb6c988d672344f02
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-08-15 15:05:25 +0200

    firstSteps

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const image = document.querySelector('.gallery');
+console.log(image.innerHTML);
+const markup = galleryItems
+    .map((image) => `<div class="gallery-item"><img src=${image.url} alt=${image.alt}></img></div>`
+)
