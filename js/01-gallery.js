9ba36eb7ba2eca6d53f85f94259ae23949894df4
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-08-18 20:40:11 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
-const image = document.querySelector('.gallery');
-console.log(image.innerHTML);
-const markup = galleryItems
-    .map((image) => `<div class="gallery-item"><img src=${image.url} alt=${image.alt}></img></div>`
-)
+
+const fotoGallery = document.querySelector(".gallery");
+const getFotoGallery = galleryItems
+	.map(
+		({preview, original, description}) => `<div class="gallery__item"><a class="gallery__link" href="${original}">
+		<img
+			class="gallery__image"
+			src="${preview}"
+			data-source="${original}"
+			alt="${description}"
+		/>
+	</a> </div>`
+	)
+	.join(" ");
+
+fotoGallery.insertAdjacentHTML('beforeend', getFotoGallery)
+
+fotoGallery.addEventListener("click", getFoto);
+
+function getFoto(event) {
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+	const modal = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`);
+
+	modal.show();
+	document.addEventListener("keydown", event => {
+		if (event.key === "Escape") {
+			modal.close();
+		}
+	});
+}
